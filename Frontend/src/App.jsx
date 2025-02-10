b4283cc23de4e772059722a8f002ef4d2b69f933
Nguyễn Lê Tấn Nghiệp <tannghiepnguyen123@gmail.com>
2025-02-10 09:04:00 +0700

    Fixing import in App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,39 +1,42 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import CreateFetus from './component/CreateFetus/CreateFetus'
-import CreateFetusHealth from './component/CreateFetusHealth/CreateFetusHealth'
-import Header from './component/Header/Header'
-import Footer from './component/Footer/Footer'
-import LoginPage from './Minh/Login';
-import RegisPage from './Minh/Register';
-import ForgetPage from './Minh/ForgetP';
-import ForgetPage2 from './Minh/ForgetP2';
+import { useEffect, useState } from "react";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import CreateFetus from "./component/CreateFetus/CreateFetus";
+import CreateFetusHealth from "./component/CreateFetusHealth/CreateFetusHealth";
+import Footer from "./component/Footer/Footer";
+import Header from "./component/Header/Header";
+import Home from "./component/Home/Home";
+import ForgetPage from "./Minh/ForgetP";
+import ForgetPage2 from "./Minh/ForgetP2";
+import LoginPage from "./Minh/Login";
+import RegisPage from "./Minh/Register";
 
 function App() {
-  const [isLogin, setIsLogin] = useState(false);
-  useEffect(() => {
-      const userToken = localStorage.getItem('userToken');
-      if (userToken) {
-          setIsLogin(true);
-      }
-  }, []);
-  return (
-    <Router>
-      <div className="app">
-        <Header />
-        <main className="main-content">
-          <Routes>
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/regis" element={<RegisPage />} />
-            <Route path="/forget" element={<ForgetPage />} />
-            <Route path="/forget2" element={<ForgetPage2 />} />
-            <Route path="/" element={<CreateFetus />} />
-            <Route path="/fetus-health" element={<CreateFetusHealth />} />
-          </Routes>
-        </main>
-        <Footer/>
-      </div>
-    </Router>
-  )
+	const [isLogin, setIsLogin] = useState(false);
+	useEffect(() => {
+		const userToken = localStorage.getItem("userToken");
+		if (userToken) {
+			setIsLogin(true);
+		}
+	}, []);
+	return (
+		<Router>
+			<div className="app">
+				<Header />
+				<main className="main-content">
+					<Routes>
+						<Route path="/login" element={<LoginPage />} />
+						<Route path="/regis" element={<RegisPage />} />
+						<Route path="/forget" element={<ForgetPage />} />
+						<Route path="/forget2" element={<ForgetPage2 />} />
+						<Route path="/" element={<Home />} />
+						<Route path="/create-fetus" element={<CreateFetus />} />
+						<Route path="/fetus-health" element={<CreateFetusHealth />} />
+					</Routes>
+				</main>
+				<Footer />
+			</div>
+		</Router>
+	);
 }
 
-export default App
+export default App;
