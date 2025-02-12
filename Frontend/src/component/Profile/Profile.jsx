9c624a0e01cf1c627579cae48a66496a175a5443
Cao Hong My Duyen <148830357+midin1507@users.noreply.github.com>
2025-02-12 20:46:59 +0700

    Update Profile.jsx
    fix profile

--- Frontend/src/component/Profile/Profile.jsx
+++ Frontend/src/component/Profile/Profile.jsx
@@ -1,66 +1,99 @@
-import React, { useEffect } from 'react';
+import  { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Layout, Input, Button, Form, Typography, Card, Row, Col, Avatar, Space, message } from 'antd';
-import { updateUserLoginAction } from '../../store/redux/reducers/userReducer';
+import { Layout, Input, Button, Form, Typography, Card, Row, Col,  Space, message, Upload } from 'antd';
+import { updateUserByIdAction } from '../../store/redux/action/userAction';
+import {changePassworbyUserIdAction} from '../../store/redux/action/userAction';
 import { fetchUserByIdAction } from '../../store/redux/action/userAction';
 
 const { Content } = Layout;
 const { Title } = Typography;
 
 const Profile = () => {
+  const userId='3e852369-e934-4184-97db-08dd4921d556';
   const dispatch = useDispatch();
-  const [form] = Form.useForm();
+  const [profileForm] = Form.useForm();
+  const [passwordForm] = Form.useForm();
   const userLogin = useSelector((state) => state.userReducer.currentUser);
+  const userDetailData = useSelector(state => state.userReducer.user);
+  const [previewAvatar, setPreviewAvatar] = useState(null);
 
   useEffect(() => {
-    if (userLogin) {
-      dispatch(fetchUserByIdAction(userLogin.userId));
+    if(userLogin) {
+      dispatch(fetchUserByIdAction(userId));
     }
   }, [dispatch, userLogin]);
 
+
   useEffect(() => {
-    if (userLogin) {
-      form.setFieldsValue({
-        roleId: userLogin?.roleId,
-        password: userLogin?.password,
-        email: userLogin?.email,
-        fullName: userLogin?.fullName,
-        phone: userLogin?.phone,
-        address: userLogin?.address,
-        imageUrl: userLogin?.imageUrl,
-        experience: userLogin?.experience,
-        status: userLogin?.status,
+    if (userDetailData) {
+      profileForm.setFieldsValue({
+        roleId: userDetailData.role?.name || '',
+        username: userDetailData?.username,
+        password: userDetailData?.password,
+        email: userDetailData?.email,
+        fullName: userDetailData?.fullName,
+        cccd: userDetailData?.cccd,
+        dateOfBirth: userDetailData?.dateOfBirth,
+        avatar: userDetailData?.avatar,
+        experience: userDetailData?.experience,
+        status: userDetailData?.status,
       });
     }
-  }, [userLogin, form]);
+  }, [userDetailData, profileForm]);
 
-  const handleSubmit = (values) => {
-    const userDetails = {
-      roleId: userLogin?.roleId,
-      password: values.password || userLogin?.password,
-      email: values.email,
-      fullName: values.fullName,
-      phone: values.phone,
-      address: values.address,
-      imageUrl: values.imageUrl,
-      experience: Number(values.experience) || 0,
-      status: true,
-    };
+  const handleSubmit = async(values) => {
+    const formData = new FormData();
+    formData.append('fullName', values.fullName);
+    formData.append('dateOfBirth', values.dateOfBirth);
 
-    dispatch(updateUserByIdAction(userLogin.userId, userDetails))
+    if (previewAvatar) {
+      const response = await fetch(previewAvatar);
+      const blob = await response.blob();
+      
+      // Lấy tên gốc từ URL nếu có
+      const fileName = previewAvatar.split('/').pop() || 'avatar.png';
+      
+      formData.append('avatar', blob, fileName);
+    }
+
+    dispatch(updateUserByIdAction(userDetailData.id, formData))
       .then(() => {
         message.success('Cập nhật thông tin thành công!');
-        dispatch(updateUserLoginAction(userDetails));
-        dispatch(fetchUserByIdAction(userLogin.userId));
+
+        dispatch(fetchUserByIdAction(userDetailData.id));
       })
       .catch((error) => {
         message.error('Cập nhật thông tin thất bại: ' + (error.message || 'Lỗi không xác định'));
       });
   };
 
-  if (!userLogin) {
-    return null; 
+  const handleSubmitPassword = (values) => {
+    const { currentPassword, newPassword, confirmPassword } = values;
+    if (newPassword !== confirmPassword) {
+      message.error('Mật khẩu mới không khớp!');
+      return;
+    }
+
+    const userDetails = {
+      currentPassword: currentPassword,
+      newPassword: newPassword,
+      confirmPassword: confirmPassword,
+
+    };
+
+    dispatch(changePassworbyUserIdAction(userDetailData.id, userDetails))
+      .then(() => {
+        message.success('Đổi mật khẩu thành công!');
+        dispatch(fetchUserByIdAction(userDetailData.id));
+      })
+      .catch((error) => {
+        message.error('Đổi mật khẩu thất bại: ' + (error.message || 'Lỗi không xác định'));
+      });
   }
+
+  // if (!userLogin) {
+  //   return null; 
+  // }
 
   return (
     <Layout style={{ minHeight: '100vh', width: '100vw' }}>
@@ -68,12 +101,29 @@ const Profile = () => {
         <Card style={{ maxWidth: 1200, margin: '0 auto', padding: 24, borderRadius: '10px', boxShadow: '0 2px 10px rgba(0,0,0,0.1)' }}>
           <Row gutter={24}>
             <Col span={8} style={{ textAlign: 'center' }}>
-              <Title level={3}>Thông tin cá nhân</Title>
-              <Avatar
-                size={120} 
-                src={userLogin?.imageUrl || 'https://via.placeholder.com/120'}
-                style={{ marginBottom: '16px', border: '2px solid #1890ff' }}
-              />
+              <Title level={3}>Ảnh đại diện</Title>
+                <Upload
+                  name="avatar"
+                  listType="picture-card"
+                  showUploadList={false}
+                  beforeUpload={(file) => {
+                    // Hiển thị ảnh mới ngay lập tức
+                    const reader = new FileReader();
+                    reader.onload = (e) => setPreviewAvatar(e.target.result);
+                    reader.readAsDataURL(file);
+
+                    // Cập nhật giá trị trong form để khi nhấn "Save", ảnh mới được gửi
+                    profileForm.setFieldsValue({ avatar: [file] });
+
+                    return false; // Ngăn chặn upload tự động
+                  }}
+                >
+                  <img 
+                    src={previewAvatar || userDetailData.avatar} 
+                    alt="avatar" 
+                    style={{ width: '100%' }} 
+                  />
+                </Upload>
               <Title level={4}>{userLogin?.fullName}</Title>
               <p>{userLogin?.email}</p>
               <p>{userLogin?.phone}</p>
@@ -82,7 +132,7 @@ const Profile = () => {
             <Col span={16}>
               <Title level={3}>Thông tin cá nhân</Title>
               <Form
-                form={form}
+                form={profileForm}
                 onFinish={handleSubmit}
                 layout="vertical"
                 style={{ backgroundColor: '#f9f9f9', padding: '20px', borderRadius: '8px' }}
@@ -95,32 +145,28 @@ const Profile = () => {
                   </Col>
                   <Col span={12}>
                     <Form.Item label="Email" name="email">
-                      <Input />
+                      <Input disabled />
                     </Form.Item>
                   </Col>
                   <Col span={12}>
-                    <Form.Item label="Số Điện Thoại" name="phone">
-                      <Input />
+                    <Form.Item label="Tên đăng nhập" name="username">
+                      <Input disabled />
+                    </Form.Item>
+                  </Col>
+                  
+                  <Col span={12}>
+                    <Form.Item label="Căn cước công dân" name="cccd">
+                      <Input disabled />
                     </Form.Item>
                   </Col>
                   <Col span={12}>
-                    <Form.Item label="Địa Chỉ" name="address">
-                      <Input />
-                    </Form.Item>
-                  </Col>
-                  <Col span={12}>
-                    <Form.Item label="Mật Khẩu" name="password">
-                      <Input.Password />
-                    </Form.Item>
-                  </Col>
-                  <Col span={12}>
-                    <Form.Item label="URL Hình Ảnh" name="imageUrl">
-                      <Input />
+                    <Form.Item label="Ngày Sinh" name="dateOfBirth">
+                      <Input type="date" />
                     </Form.Item>
                   </Col>
                   <Col span={12}>
                     <Form.Item label="Kinh Nghiệm" name="experience">
-                      <Input type="number" />
+                      <Input type="number" disabled/>
                     </Form.Item>
                   </Col>
                   <Col span={12}>
@@ -130,7 +176,7 @@ const Profile = () => {
                   </Col>
                   <Col span={12}>
                     <Form.Item label="Trạng Thái" name="status">
-                      <Input disabled />
+                      <Input disabled  />
                     </Form.Item>
                   </Col>
                 </Row>
@@ -141,6 +187,39 @@ const Profile = () => {
                     </Button>
                   </Space>
                 </Form.Item>
+              </Form>
+              <Title level={3}>Đổi mật khẩu</Title>
+              <Form
+                form={passwordForm}
+                onFinish={handleSubmitPassword}
+                layout="vertical"
+                style={{ backgroundColor: '#f9f9f9', padding: '20px', borderRadius: '8px' }}
+              >
+                <Row gutter={16}>
+                  <Col span={12}>
+                    <Form.Item label="Mật Khẩu Cũ" name="currentPassword">
+                      <Input.Password />
+                    </Form.Item>
+                  </Col>
+                  <Col span={12}>
+                    <Form.Item label="Mật Khẩu Mới" name="newPassword">
+                      <Input.Password />
+                    </Form.Item>
+                  </Col>
+                  <Col span={12}>
+                    <Form.Item label="Nhập Lại Mật Khẩu" name="confirmPassword">
+                      <Input.Password />
+                    </Form.Item>
+                  </Col>
+                </Row>
+                <Form.Item>
+                  <Space style={{ display: 'flex', justifyContent: 'center' }}>
+                    <Button type="primary" htmlType="submit" style={{ width: '100%' }}>
+                      Lưu
+                    </Button>
+                  </Space>
+                </Form.Item>
+
               </Form>
             </Col>
           </Row>
